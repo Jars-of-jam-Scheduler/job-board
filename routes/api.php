3e5a923e0418230f57fd1e8c3943eb1873b0ff6b
Jars <anon>
2022-12-16 10:08:21 +0100

    Regroup routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,4 @@
 <?php
-
 use App\Models\User;
 use App\Http\Controllers\{UserController, JobController, ApplierController, FirmController, EnumController};
 
@@ -36,30 +35,34 @@ Route::post('/sanctum/token', function(Request $request) {
 	return $user->createToken($request->device_name)->plainTextToken;
 });
 
-Route::apiResource('jobs', JobController::class);  // Routes are Sanctumed in the controller
+Route::apiResource('jobs', JobController::class);  // JobController: Routes are Sanctumed in the controller
 Route::put('/jobs/{job}/restore', [JobController::class, 'restore'])->whereNumber('job')->name('jobs_restore');
 
 Route::middleware('auth:sanctum')->group(function() {
+	/* <!-- Enums --> */
+	Route::get('/enums', [EnumController::class, 'get'])->name('enums_get');
+
+	/* <!-- User --> */
 	Route::get('/user', [UserController::class, 'show'])->name('user_show');
 	Route::post('/user/logout', function(Request $request) {
 		auth()->user()->tokens()->delete();
 	})->name('user_logout');
 
-	Route::get('/enums', [EnumController::class, 'get'])->name('enums_get');
-});
+	/* <!-- Appliers --> */
+	Route::prefix('appliers')->name('appliers.')->group(function() {
+		Route::put('/', [ApplierController::class, 'update'])->name('update');
+		Route::put('/jobs/{job}/attach', [ApplierController::class, 'attachJob'])->whereNumber('job')->name('attach_job');
+		Route::put('/jobs/{job}/detach', [ApplierController::class, 'detachJob'])->whereNumber('job')->name('detach_job');
+	});
 
-Route::middleware('auth:sanctum')->prefix('appliers')->name('appliers.')->group(function() {
-	Route::put('/', [ApplierController::class, 'update'])->name('update');
-	Route::put('/jobs/{job}/attach', [ApplierController::class, 'attachJob'])->whereNumber('job')->name('attach_job');
-	Route::put('/jobs/{job}/detach', [ApplierController::class, 'detachJob'])->whereNumber('job')->name('detach_job');
-});
+	/* <!-- Firms and Firm --> */
+	Route::prefix('firms')->name('firms.')->group(function() {
+		Route::put('/', [FirmController::class, 'update'])->name('update');
+		Route::post('/jobs_applications/{job_application}/accept_or_refuse_job_application', [FirmController::class, 'acceptOrRefuseJobApplication'])->whereNumber('job_application')->name('accept_or_refuse_job_application');
+	});
 
-Route::middleware('auth:sanctum')->prefix('firms')->name('firms.')->group(function() {
-	Route::put('/', [FirmController::class, 'update'])->name('update');
-	Route::post('/jobs_applications/{job_application}/accept_or_refuse_job_application', [FirmController::class, 'acceptOrRefuseJobApplication'])->whereNumber('job_application')->name('accept_or_refuse_job_application');
-});
-
-Route::middleware('auth:sanctum')->prefix('firm')->name('firm.')->group(function() {
-	Route::get('/jobs', [FirmController::class, 'getJobs'])->name('get_jobs');
-	Route::get('/soft_deleted_jobs', [FirmController::class, 'getSoftDeletedJobs'])->name('get_soft_deleted_jobs');
+	Route::prefix('firm')->name('firm.')->group(function() {
+		Route::get('/jobs', [FirmController::class, 'getJobs'])->name('get_jobs');
+		Route::get('/soft_deleted_jobs', [FirmController::class, 'getSoftDeletedJobs'])->name('get_soft_deleted_jobs');
+	});
 });
